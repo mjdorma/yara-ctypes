cad75349af2378b3cdfde145a8d504b25142f59b
mjdorma <mjdorma+github@gmail.com>
2013-04-11 10:36:39 +1000

    testing list and rule params

--- tests/test_scan.py
+++ tests/test_scan.py
@@ -1,21 +1,31 @@
 import unittest
 from StringIO import StringIO 
 import sys
+import os
 
 from yara import scan
 
+TEST_ROOT = os.path.abspath(os.path.dirname(__file__))
 
 class TestScanNamespace(unittest.TestCase):
 
     def test_list(self):
+        sys.stdout = StringIO()
+        try:
+            scan.main(['--list'])
+            sys.stdout.seek(0)
+            out = sys.stdout.read()
+        finally:
+            sys.stdout = sys.__stdout__
+        self.assertTrue("example.packer_rules" in out)
+
+    def test_yara_file(self):
         sys.stderr = StringIO()
         try:
-            scan.main('--list')
+            scan.main(['-r', os.path.join(TEST_ROOT, 'rules.yar')])
             sys.stderr.seek(0)
             out = sys.stderr.read()
         finally:
             sys.stderr = sys.__stderr__
-        self.assertTrue("example.packer_rules" in out)
-
-
-
+        
+        self.assertTrue("does not exist" not in out)

aa53d0e184fd20b67e0600b6a4382eb7292c5ff1
mjdorma <mjdorma+github@gmail.com>
2013-04-13 23:08:54 +1000

    moved main to cli

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ setup(
     install_requires = [],
     entry_points={
         'console_scripts': [
-            'yara-ctypes = yara.scan:entry'
+            'yara-ctypes = yara.cli:entry'
             ]
     },
     platforms=['cygwin', 'win', 'linux'],

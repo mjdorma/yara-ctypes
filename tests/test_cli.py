61753d81ef7e1c693a0edcee4500343f427fabe8
mjdorma <mjdorma+github@gmail.com>
2013-04-14 11:08:11 +1000

    more tests.... more coverage :)

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,6 +10,7 @@ from yara import cli
 
 RULES_ROOT = os.path.join(os.path.abspath(os.path.dirname(__file__)), 'rules')
 BIRD_YAR = os.path.join(RULES_ROOT, 'bird', 'meta.yar')
+EXTERN_YAR = os.path.join(RULES_ROOT, 'extern.yar')
 
 def run_main(*args):
     sys.stdout = StringIO()
@@ -32,6 +33,11 @@ class TestScanNamespace(unittest.TestCase):
         self.assertEqual(ret, 0)
         stdout = stdout.strip()
         self.assertTrue(stdout.startswith('NAME scan'))
+
+    def test_broken_opt(self):
+        ret, stdout, stderr = run_main('--no-an-opt-opt')
+        self.assertEqual(ret, -1)
+        self.assertTrue("Getopt error:" in stderr)
 
     def test_list(self):
         ret, stdout, stderr = run_main('--list')
@@ -74,13 +80,68 @@ class TestScanNamespace(unittest.TestCase):
     def test_simple(self):
         ret, stdout, stderr = run_main('-r', BIRD_YAR, '--simple', BIRD_YAR)
         self.assertEqual(ret, 0)
-        print stdout
-        print stderr
+        self.assertTrue("rules/bird/meta.yar: main.Bird01" in stdout)
 
+    def test_root(self):
+        ret, stdout, stderr = run_main('--root=doesnotexit')
+        self.assertEqual(ret, -1)
+        self.assertTrue("does not exist" in stderr)
 
+        ret, stdout, stderr = run_main('--root=%s' % RULES_ROOT, '--list')
+        self.assertEqual(ret, -1)
+        self.assertTrue("You could blacklist the erroneous " in stderr)
 
+    def test_file_chunks(self):
+        ret, stdout, stderr = run_main('-r', BIRD_YAR, 
+                            '--file-chunk-size=10', BIRD_YAR)
+        self.assertTrue("rules/bird/meta.yar[150:160]" in stdout)
+        self.assertEqual(ret, 0)
 
+        ret, stdout, stderr = run_main('-r', BIRD_YAR, 
+                            '--file-chunk-size=10', 
+                            '--file-readahead-limit=20', BIRD_YAR)
+        self.assertTrue("rules/bird/meta.yar[150:160]" in stdout)
+        self.assertEqual(ret, 0)
 
+        ret, stdout, stderr = run_main('--file-chunk-size=a')
+        self.assertEqual(ret, -1)
+        self.assertEqual("param 'a' was not an int", stderr.strip())
 
+        ret, stdout, stderr = run_main('--file-readahead-limit=a')
+        self.assertEqual(ret, -1)
+        self.assertEqual("param 'a' was not an int", stderr.strip())
+
+    def test_threadpool(self):
+        ret, stdout, stderr = run_main('--thread-pool=a')
+        self.assertEqual(ret, -1)
+        self.assertEqual("param 'a' was not an int", stderr.strip())
+
+        ret, stdout, stderr = run_main('--thread-pool=0')
+        self.assertEqual(ret, -1)
+        self.assertEqual("--thread-pool value can not be lower than 1",
+                            stderr.strip())
+
+    def test_externals(self):
+        ret, stdout, stderr = run_main('-r', EXTERN_YAR, 
+                '-d', 'ext_int_var=4', '-d', 'ext_bool_var=True', 
+                '-d', 'ext_str_var="false"', BIRD_YAR)
+        self.assertEqual(ret, 0)
+        self.assertTrue("TestExternBool" in stdout)
+
+        ret, stdout, stderr = run_main('-r', EXTERN_YAR, 
+                '-d', 'ext_int_var=4', '-d', 'ext_bool_var=False', 
+                '-d', 'ext_str_var="test"', BIRD_YAR)
+        self.assertEqual(ret, 0)
+        self.assertTrue("TestExternStr" in stdout)
+
+        ret, stdout, stderr = run_main('-r', EXTERN_YAR, 
+                '-d', 'ext_int_var=10', '-d', 'ext_bool_var=False', 
+                '-d', 'ext_str_var="false"', BIRD_YAR)
+        self.assertEqual(ret, 0)
+        self.assertTrue("TestExternInt" in stdout)
+
+        ret, stdout, stderr = run_main('-d', '44 broken')
+        self.assertEqual(ret, -1)
+        self.assertEqual("external '44 broken' syntax error", stderr.strip())
 
 

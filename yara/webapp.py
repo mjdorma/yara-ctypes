9de4a197a3ed4b41afcd8b1006c1d3034c8f34c1
mjdorma <mjdorma+github@gmail.com>
2013-04-23 06:02:51 +1000

    added main for boot strap

--- yara/webapp.py
+++ yara/webapp.py
@@ -4,18 +4,17 @@ from threading import Lock
 
 import bottle
 
-import scan
-
 MAX_POST_SIZE = 2**20 * 100 # 100 MB
 
-@bottle.post("scan")
+@bottle.post("/yara/scan")
 def scan(self):
+    global scanner
     filenames = []
     data_list = []
     bytes_remaining = MAX_POST_SIZE
     for filename, field in request.files.iteritems():
         if bytes_remaining <= 0:
-            bottle.abbort(httplib.REQUEST_ENTITY_TOO_LARGE, 
+            bottle.abort(httplib.REQUEST_ENTITY_TOO_LARGE, 
                     'POST was > %s bytes' % MAX_POST_SIZE)
         d = field.file.read(bytes_remaining)
         bytes_remaining -= len(data)
@@ -30,4 +29,8 @@ def scan(self):
     return results 
     
 
-#self.scanner = scan.SyncScanner(**scanner_kwargs)
+def run(scanner, **bottle_run_kwargs):
+    global scanner
+    scanner = scanner
+    bottle.run(**bottle_run_kwargs)
+
